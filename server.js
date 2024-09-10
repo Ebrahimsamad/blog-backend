bcd2f458ef015aac63a574b7a5dc6a74de27c54d
Ebrahimsamad <ebrahim7asn@gmail.com>
2024-09-10 23:31:38 +0300

    add cors

--- server.js
+++ server.js
@@ -11,13 +11,7 @@ const app = express();
 const cors = require("cors");
 const authMiddleware = require("./middlewares/authMiddleware");
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
